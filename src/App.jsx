852b04df29a663526a5206be829afb286fbc0289
sylvester <sylvestermbah90@gmail.com>
2026-06-01 06:50:03 -0700

    Add UI with styling and Cv submit and edit logics

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,33 @@ function App() {
       <div className="mx-auto max-w-5xl rounded-xl bg-white p-6 shadow-lg">
         <h1 className="mb-6 text-center text-4xl font-bold text-gray-100">CV Application</h1>
 
-        {}
+        {!isSubmitted ? (
+          <form action="#" onSubmit={handleSubmit} className="space-y-8">
+            <GeneralInfo generalInfo={generalInfo} setGeneralInfo={setGeneralInfo} />
+
+            <Education education={education} setEducation={setEducation} />
+
+            <Experience experience={experience} setExperience={setExperience} />
+
+            <button type="submit" className="w-full rounded-lg bg-black py-3 text-white transition hover:bg-gray-800">
+              Submit CV
+            </button>
+          </form>
+        ) : (
+          <div>
+            <CvPreview 
+              generalInfo={generalInfo}
+              education={education}
+              experience={experience}
+            />
+
+            <button 
+              onClick={handleEdit} 
+              className="mt-6 w-full rounded-lg bg-blue-600 py-3 text-white transition hover:bg-blue-700">
+              Edit CV
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
