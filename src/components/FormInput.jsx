420697baf043a98f733bdeeafded4d9ce8af7e20
sylvester <sylvestermbah90@gmail.com>
2026-07-15 06:47:50 -0700

    Create reusable FormInput component

---
+++ src/components/FormInput.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+
+const FormInput = ({ type = 'text', name, placeholder, value, onChange }) => {
+  return (
+    <div>
+      <input 
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className='w-full rounded-xl border border-b-gray-300 bg-white px-4 py-3 text-gray-700
+          placeholder:text-gray-400 shadow-sm transition-all duration-200 outline-none focus:border-blue-500
+          focus:ring-2 focus:ring-blue-200'
+      />
+    </div>
+  )
+}
+
+FormInput.propTypes = {
+  type: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default FormInput
