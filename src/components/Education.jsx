37478b9eda0bc5538ae07669ec97d18292ba965f
sylvester <sylvestermbah90@gmail.com>
2026-07-15 07:33:56 -0700

    Use reusable components in Education section

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,39 +1,52 @@
+import PropTypes from "prop-types";
+import FormInput from './FormInput';
+import SectionTitle from "./SectionTitle";
+
+
 export default function Education({ education, setEducation }) {
 
     function handleChange(e) {
         setEducation((prev) => ({...prev, [e.target.name]: e.target.value}));
     }
-    return (
-        <section className="rounded-lg border p-5">
-            <h2 className="mb-4 text-2xl font-semibold">Educational Experience</h2>
-            <div className="flex flex-col gap-4 md:flex-row">
 
-                <input 
-                  type="text" 
+    return (
+        <section className="rounded-2xl border border-gray-200 bg-white p-6 shadow-md">
+            <SectionTitle 
+              title={'Educational Experience'}
+            />
+
+            <div className="flex flex-col gap-4">
+
+                <FormInput 
                   name="school"
                   value={education.school}
                   placeholder="School Name"
                   onChange={handleChange}
-                  className="rounded-lg border p-3 outline-none focus:ring-2 focus:ring-black"
                 />
 
-                <input 
-                  type="text"
+                <FormInput
                   name="study"
                   placeholder="Title of Study"
                   value={education.study}
                   onChange={handleChange}
-                  className="rounded-lg border p-3 outline-none focus:ring-2 focus:ring-black"
                 />
 
-                <input 
+                <FormInput 
                   type="date" 
                   name="date"
                   value={education.date}
                   onChange={handleChange}
-                  className="rounded-lg border p-3 outline-none focus:ring-2 focus:ring-black"
                 />
             </div>
         </section>
     )
 }
+
+Education.propTypes = {
+    education: PropTypes.shape({
+        school: PropTypes.string.isRequired,
+        study: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired,
+    }).isRequired,
+    setEducation: PropTypes.func.isRequired,
+};
